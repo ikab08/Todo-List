3c46855b148a84ae4220719f674062301cd11c43
Khadim IBK <khadim.ibk@gmail.com>
2025-08-22 19:38:59 +0100

    upd script

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 const taskInput = document.querySelector("#taskInput");
-const addTask = document.querySelector("#addTask");
+const addBtn = document.querySelector("#addTask");
 const taskList = document.querySelector("#taskList");
 
-addTask.addEventListener('click', ()=> {
-  const valTaskInput = taskInput.value;
-  if(valTaskInput) {
-    const li = document.createElement('li');
-    const delBtn = document.createElement('button');
-    delBtn.classList.add("delete-btn");
-    delBtn.textContent = "X";
-    li.textContent = valTaskInput+' ';
-    taskList.appendChild(li).appendChild(delBtn);
+addBtn.addEventListener('click', ()=> {
+  const valTask = taskInput.value;
+  if (!valTask.trim()) return;    // ignore si vide
+  
+  const li = document.createElement('li');
+  li.textContent = valTask;
 
-    document.querySelectorAll(".delete-btn").forEach(btn => {
-      btn.addEventListener("click", ()=> {
-        li.remove();
-      })
-    }); 
-  }    
+  // bouton supprimer
+  const del = document.createElement('button');
+  del.classList.add("delete-btn");
+  del.textContent = "X";
+  del.addEventListener('click', ()=>{
+    li.remove();
+  })
+
+  li.appendChild(del);
+  taskList.appendChild(li);
+  taskInput.value = "";   
 })
 
 
